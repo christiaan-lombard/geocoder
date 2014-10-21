23b94124df03b6bcb55630b16be10dd7113740ff
christiaan-lombard <christiaan.lombard@yousemble.com>
2014-10-21 09:31:38 +0200

    better filenames

---
+++ src/Yousemble/Geocoder/GeocoderService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Yousemble\Geocoder;
+
+use Geocoder\GeocoderInterface as GeocoderContract;
+
+
+class GeocoderService
+{
+
+  private $geocoder;
+
+  function __construct(GeocoderContract $geocoder){
+
+
+
+  }
+}
